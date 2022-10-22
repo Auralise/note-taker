c383e80f2e121b871a6a24a8d782d4fb19be9335
James Prince <james.prince1@gmail.com>
2022-10-22 15:47:37 +1100

    Added delete routes. Added additional status report calls for logging

--- routes/page-routes.js
+++ routes/page-routes.js
@@ -4,18 +4,17 @@ const pageRoutes = express.Router();
 const {statusReport} = require('../utils/logger');
 
 
-
-pageRoutes.get('/', (req, res) => {
-    const pagePath = path.join(__dirname, '../public/index.html');
-    
-    res.sendFile(pagePath);
-});
-
 pageRoutes.get('/notes', (req, res)=> {
     const pagePath = path.join(__dirname, '../public/notes.html');
+    statusReport(200, );
     res.sendFile(pagePath);
 });
 
+pageRoutes.get('*', (req, res) => {
+    const pagePath = path.join(__dirname, '../public/index.html');
+    statusReport(200, );
+    res.sendFile(pagePath);
+});
 
 
 
